c5ddcd8605a7a18c8540ceded3c215aabd2d99a2
Glitch (vaporwave-discord) <none>
2017-12-16 09:40:06 +0000

    new Kick and Ban command

--- README.md
+++ README.md
@@ -10,4 +10,5 @@ https://github.com/ajv316/vaporwave
 #
 
 ### Updates
+- 12/16/17 = New updates = Kick and Ban command! (~~ban, ~~kick)
 - 12/15/17 = New say Command (~~say)
--- server.js
+++ server.js
@@ -11,7 +11,16 @@ client.on('ready', () => {
   client.user.setPresence({ game: {name: "do ~~help"}})
 });
 
-// Command Handler
+client.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). The guild has ${guild.memberCount} members`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+});
+
+
+// Command Handler | Commands
 client.on("message", message => {
   if (message.author.bot) return;
   if(message.content.indexOf(Prefix) !== 0) return;
@@ -56,9 +65,47 @@ client.on("message", message => {
     message.delete()
     message.channel.send(`${args.join(" ")}`)
   }
-  // Mute cmd
-  if (command === "mute") {
+  // Kick command
+  if (command === "kick") {
+    // Scan if the member is kickable
+      if(!message.member.roles.some(r=>["Administrator", "Moderator", "Mod", "Admin", "mod", "admin", "Owner", "owner"].includes(r.name)) )
+      return message.reply("You don't have the permission to use this command ~_~");
     
+        let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("You need to mention someone to kick ~_~");
+    if(!member.kickable) 
+      return message.reply("Cannot kick user beacause he/she might be : **Higher than me, an error has occured, I don't have the permission to kick** ~_~ ");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("You need a reason for the kick ~_~");
+
+    member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} Cannnot kick user because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because of ${reason} ~_~`);
+
+  }
+  // Ban Command
+   if(command === "ban") {
+    // Most of this command is identical to kick, except that here we'll only let admins do it.
+    // In the real world mods could ban too, but this is just an example, right? ;)
+    if(!message.member.roles.some(r=>["Administrator", "Moderator", "Mod", "Admin", "mod", "admin", "Owner", "owner"].includes(r.name)) )
+      return message.reply("You don't have the permission to use this command ~_~");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("You need to mention someone to ban ~_~");
+    if(!member.bannable) 
+      return message.reply("Cannot ban user beacause he/she might be : **Higher than me, an error has occured, I don't have the permission to ban** ~_~ ");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("You need a reason for the kick ~_~");
+    
+    member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because of ${reason} ~_~`);
   }
 });  
 
